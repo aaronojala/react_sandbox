e30f5bb87a5177cbd8fc955fb6da90268f105564
Aaron Ojala <aaronojala@yahoo.com>
2020-10-29 15:02:51 +0200

    added footer and header

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import './App.css';
 
 class App extends Component {
+  state = {
+    likes: 0,
+  };
+
+addHandler = () =>{
+  console.log('addHandler was clicked');
+  this.setState({ likes: this.state.likes + 1});
+};
+removeHandler = () =>{
+  console.log('removeHandler was clicked');
+  this.setState({ likes: this.state.likes - 1});
+};
+resetHandler = () =>{
+  console.log('resetHandler was clicked');
+  this.setState({ likes: 0});
+};
+
   render() {
     return (
-      <div>
-        <h1>Total likes: </h1>
+      <div className="likesCounter">
+        <Header />
+        <div className="likes">
+        <h1>Total likes: {this.state.likes}</h1>
+        <button onClick={this.addHandler}>Add like</button>
+        <button onClick={this.removeHandler}>Remove like</button>
+        <button onClick={this.resetHandler}>Reset</button>  
+        </div>
+        <Footer />
       </div>
     );
   }
-}
+};
 
 export default App;
