72c62a768942c9880dd20ad1d723da4e2bf07baf
Aaron Ojala <aaronojala@yahoo.com>
2020-10-29 13:31:22 +0200

    changed function components to class component

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Animal from './Components/Animal/Animal'
-import './App.css'
+import React, {Component} from 'react';
+import Animal from './Components/Animal/Animal';
+import './App.css';
 
-const App = () => {
-  return (
-    <div className="cards">
-      <Animal name="Dog" img="https://source.unsplash.com/UtrE5DcgEyg" />  
-      <Animal name="Monkey" img="https://source.unsplash.com/4B5F388MLE4" />  
-      <Animal name="Elephant" img="https://source.unsplash.com/Twm64rH8wdc" />    
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    animals: [
+      {name: "Doggy", img: "https://source.unsplash.com/UtrE5DcgEyg"},
+      {name: "Monkey", img: "https://source.unsplash.com/4B5F388MLE4"},
+      {name: "Elephant", img: "https://source.unsplash.com/Twm64rH8wdc"},
+    ],
+  };
+  render(){
+    return (
+      <div className="cards">
+      <Animal 
+      name={this.state.animals[0].name}
+      img={this.state.animals[0].img}
+      />     
+      <Animal 
+      name={this.state.animals[1].name}
+      img={this.state.animals[1].img}
+      />     
+      <Animal 
+      name={this.state.animals[2].name}
+      img={this.state.animals[2].img}
+      />     
+      </div>
+    );
+  }
+}
 
 export default App;
