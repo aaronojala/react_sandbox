67e0aace906f326cd161534b68518c7abd013455
Aaron Ojala <aaronojala@yahoo.com>
2020-10-29 13:04:13 +0200

    added animal cards with alerts

--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Animal from './Components/Animal/Animal'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>  
+    <div className="cards">
+      <Animal name="Dog" img="https://source.unsplash.com/UtrE5DcgEyg" />  
+      <Animal name="Monkey" img="https://source.unsplash.com/4B5F388MLE4" />  
+      <Animal name="Elephant" img="https://source.unsplash.com/Twm64rH8wdc" />    
     </div>
   );
 };
--- 2_task/src/Components/Animal.js
+++
@@ -1,9 +0,0 @@
-function Animal() {
-    return (
-        <div className="Animal">
-        <h2>Monkey </h2>,
-        <img src="source.unsplash.com/2k1gHogDolU"></img>,
-        <button onClick="alert('Hello I am monkey')"></button>
-        </div>
-    )
-}
