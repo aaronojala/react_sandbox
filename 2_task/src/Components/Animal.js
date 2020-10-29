fcddee85b5c6fbbbad3d249f7a9ec0a367cf339b
Aaron Ojala <aaronojala@yahoo.com>
2020-10-29 12:14:38 +0200

    add changes from thursday lesson

---
+++ 2_task/src/Components/Animal.js
@@ -0,0 +1,9 @@
+function Animal() {
+    return (
+        <div className="Animal">
+        <h2>Monkey </h2>,
+        <img src="source.unsplash.com/2k1gHogDolU"></img>,
+        <button onClick="alert('Hello I am monkey')"></button>
+        </div>
+    )
+}
