abbdfdf126b85b9868e52a3e40927cafcfbcd613
Aaron Ojala <aaronojala@yahoo.com>
2020-11-02 23:06:59 +0200

    list with map function and added key ids

--- 1_boxes/src/App.js
+++ 1_boxes/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Box from './Components/Box';
 
-function App() {
+class App extends Component {
+  state = {
+    boxes: [
+      {id: 1, name: "Aaron", title: "Big Boss", age: "29"},
+      {id: 2, name: "Suvi", title: "Team Lead", age: "29"},
+      {id: 3, name: "Bob", title: "New Guy", age: "19"},
+      {id: 4, name: "Mark", title: "Designer", age: "25"},
+      {id: 5, name: "Pekka", title: "Wizard", age: "60"},
+    ],
+  };
+
+  render() {
+    const boxesList = this.state.boxes.map(
+      (box) => {
+        return <Box key={box.id} name={box.name} title={box.title} age={box.title} />
+      });
+    return <div className="App"> {boxesList} </div>;
+  }
+}
+
+export default App;
+
+/* function App() {
   return (
     <div className="App">
     <Box name="Aaron" title="Big Boss" age="29"/>  
@@ -14,4 +36,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
