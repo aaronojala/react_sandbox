1ec454d9ab79e129078cf184819cd4d11d943c6c
Aaron Ojala <aaronojala@yahoo.com>
2020-11-26 13:00:12 +0200

    added 7 task with axios posts page

---
+++ 7_task/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Business College Helsinki</h1>
+      Weather will be here
+    </header>
+  );
+};
+
+export default Header;
